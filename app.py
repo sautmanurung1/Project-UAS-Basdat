c0832a8f08e8cd81d9e0cdea0a48deefbc1f8f7c
sautmanurung1 <smanurung360@gmail.com>
2022-12-30 11:55:02 +0700

    update code app.py

--- app.py
+++ app.py
@@ -17,12 +17,33 @@ def dashboard():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE email = % s AND password = % s', (email, password, ))
+        users = cursor.fetchone()
+        if users:
+            session['loggedin'] = True
+            session['id'] = users['id']
+            session['email'] = users['email']
+            msg = 'Logged in Successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect email / password !'
+    return render_template('login.html', msg = msg)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
